b975890664d8bb80763768e03701a59e4da041f3
Jeremy Leahy <jeremyleahy77@gmail.com>
2022-03-26 09:31:23 +0000

    Joi validation and error reporting implemented

--- src/controllers/coffee-shop-controller.js
+++ src/controllers/coffee-shop-controller.js
@@ -1,9 +1,11 @@
 import { db } from "../models/db.js";
+import { InfoSpec } from "../models/joi-schemas.js";
 
+let coffeeShop;
 export const coffeeShopController = {
   index: {
     handler: async function (request, h) {
-      const coffeeShop = await db.coffeeShopStore.getCoffeeShopById(request.params.id);
+      coffeeShop = await db.coffeeShopStore.getCoffeeShopById(request.params.id);
       const viewData = {
         title: "Coffee Shop",
         coffeeShop: coffeeShop,
@@ -13,18 +15,32 @@ export const coffeeShopController = {
   },
 
   addInfo: {
+    validate: {
+      payload: InfoSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        const viewData = {
+          title: "Add info error",
+          errors: error.details,
+          coffeeShop: coffeeShop,
+        };
+        return h.view("coffee-shop-view", viewData).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
-      const coffeeShop = await db.coffeeShopStore.getCoffeeShopById(request.params.id);
+      coffeeShop = await db.coffeeShopStore.getCoffeeShopById(request.params.id);
       const newInfo = {
-        title: request.payload.title,
+        placeName: request.payload.placeName,
+        description: request.payload.description,
       };
       await db.infoStore.addInfo(coffeeShop._id, newInfo);
       return h.redirect(`/coffeeshop/${coffeeShop._id}`);
     },
   },
+
   deleteInfo: {
     handler: async function (request, h) {
-      const coffeeShop = await db.coffeeShopStore.getCoffeeShopById(request.params.id);
+      coffeeShop = await db.coffeeShopStore.getCoffeeShopById(request.params.id);
       await db.infoStore.deleteInfo(request.params.infoid);
       return h.redirect(`/coffeeshop/${coffeeShop._id}`);
     },
---
+++ src/models/joi-schemas.js
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+export const UserSpec = {
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const UserCredentialsSpec = {
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const InfoSpec = {
+  placeName: Joi.string().required(),
+  description: Joi.string().required(),
+};
+
+export const CoffeeShopSpec = {
+  title: Joi.string().required(),
+};
